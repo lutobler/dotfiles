c9f38d37f6d17f02c8ebb5b2afd518ea5b9d8dd2
Lukas Tobler <luk4s.tobler@gmail.com>
2017-11-11 01:41:23 +0100

    Update

--- stow/vis/.config/vis/visrc.lua
+++ stow/vis/.config/vis/visrc.lua
@@ -1,13 +1,15 @@
-require("vis")
-require("plugins/filetype")
-require("plugins/complete-word")
+-- default plugins
+require 'vis'
+require 'plugins/filetype'
+require 'plugins/complete-word'
 
--- 3rd party plugins
-package.path = package.path..';'..os.getenv('HOME')..'/.config/vis/?/?.lua'
-require("vis-commentary")
--- require("vis-git-status")
-require("vis-surround")
-local modeline = require("vis-modelines")
+-- plugins
+local plug = require 'vis-plug'
+plug:load_plugins({
+    'lutobler/vis-commentary',
+    'lutobler/vis-modelines',
+    'lutobler/vis-surround'
+})
 
 local cschemes = { "base16-eighties", "papercolor" }
 local i, n = 1, #cschemes
@@ -16,22 +18,18 @@ local function ctoggle() -- cycle through the colorschemes in 'cschemes'
     vis:command("set theme " .. cschemes[i])
 end
 
-local function event_init()
+vis.events.subscribe(vis.events.INIT, function()
     vis:command("set theme "..cschemes[1])
-end
+end)
 
-local function event_win_settings(win)
+vis.events.subscribe(vis.events.WIN_OPEN, function()
     vis:command("set autoindent on")
     vis:command("set numbers on")
     vis:command("set expandtab on")
     vis:command("set cursorline on")
     vis:command("set tabwidth 4")
     vis:command("set colorcolumn 80")
-end
-
-vis.events.subscribe(vis.events.INIT,     event_init)
-vis.events.subscribe(vis.events.WIN_OPEN, event_win_settings)
-vis.events.subscribe(vis.events.WIN_OPEN, modeline.event_read_modeline)
+end)
 
 -- toggle color schemes
 vis:map(vis.modes.NORMAL,      "<F5>", ctoggle, "Toggle two colorschemes")

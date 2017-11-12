339cd326a2e514b1701ceea08548cbf6952b5a98
Lukas Tobler <luk4s.tobler@gmail.com>
2017-11-12 22:20:35 +0100

    Update

--- stow/herbstluftwm/.config/herbstluftwm/hl_workspaces.sh
+++ stow/herbstluftwm/.config/herbstluftwm/hl_workspaces.sh
@@ -4,8 +4,10 @@ greenfg="%{F#9FBC00}"
 greenbg="%{B#9FBC00}"
 redfg="%{F#ff0000}"
 redbg="%{B#ff0000}"
-background="%{B#222}"
-foreground="%{F#222}"
+blackbg="%{B#222}"
+blackfg="%{F#222}"
+greyfg="%{F#8c8c8c}"
+greybg="%{B#8c8c8c}"
 reset="%{F-}%{B-}"
 
 output=
@@ -13,16 +15,16 @@ IFS=$'\t' read -ra tags <<< "$(herbstclient tag_status)"
 for i in "${tags[@]}" ; do
     case ${i:0:1} in
         '#') # viewed on the specified monitor and focused
-            output+="$greenbg$foreground ${i:1} $reset"
+            output+="$greenbg$blackfg ${i:1} $reset"
             ;;
         ':') # not empty
-            output+="$background ${i:1} $reset"
+            output+="$blackbg ${i:1} $reset"
             ;;
         '!') # contains an urgent window
-            output+="$redbg ${i:1} $reset"
+            output+="$redbg$blackfg ${i:1} $reset"
             ;;
         *) # anything else (especially empty tags)
-            output+="$background ${i:1} $reset"
+            output+="$blackbg$greyfg ${i:1} $reset"
             ;;
     esac
 done
